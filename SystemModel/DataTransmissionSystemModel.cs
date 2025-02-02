d1323be15bef778778f780387f8e14144096167e
dmitriidemidov <dmitri-demidiv@yandex.ru>
2025-02-02 16:08:00 +0300

    IGenerator is added

--- SystemModel/DataTransmissionSystemModel.cs
+++ SystemModel/DataTransmissionSystemModel.cs
@@ -4,6 +4,16 @@ namespace SystemModel;
 
 public class DataTransmissionSystemModel : IDataTransmissionSystemModel
 {
+    private readonly IGenarator _generator;
+
+    public DataTransmissionSystemModel(
+        IGenerator generator
+    ) 
+    {
+        _generator = generator;
+    }
+    
+
     public void Model()
     {
         throw new NotImplementedException();
